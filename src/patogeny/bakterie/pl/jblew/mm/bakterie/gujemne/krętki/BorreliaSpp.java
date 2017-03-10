abd8e757127a1c88f48ec90aa26dea8a3e11b49d
Jblew <jedrzejblew@gmail.com>
2017-03-10 11:37:29 +0100

    Poprawiłem krętki

--- src/patogeny/bakterie/pl/jblew/mm/bakterie/gujemne/krętki/BorreliaSpp.java
+++ src/patogeny/bakterie/pl/jblew/mm/bakterie/gujemne/krętki/BorreliaSpp.java
@@ -1,8 +1,10 @@
 package pl.jblew.mm.bakterie.gujemne.krętki;
 
+import pl.jblew.mm.dist.Image;
+
 public class BorreliaSpp extends Krętek {
 	public static class BorreliaBurgdorferi extends BorreliaSpp {
-		@Image(url="Borrelia_burgdorferi.jpg") /*
+		@Image(url="Borrelia_burgdorferi_.jpg") /*
 		
 		
 		
--- src/patogeny/bakterie/pl/jblew/mm/bakterie/gujemne/krętki/Krętek.java
+++ src/patogeny/bakterie/pl/jblew/mm/bakterie/gujemne/krętki/Krętek.java
@@ -1,6 +1,5 @@
 package pl.jblew.mm.bakterie.gujemne.krętki;
 
-import pl.jblew.mm.bakterie.Bakteria;
 import pl.jblew.mm.bakterie.gujemne.BakteriaGramUjemna;
 
 public class Krętek extends BakteriaGramUjemna {
