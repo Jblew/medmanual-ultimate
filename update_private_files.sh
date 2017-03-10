d425187f51dc5834a027fcfaa1fdac71e0c862f0
Jblew <jedrzejblew@gmail.com>
2017-03-10 14:03:21 +0100

    Update privates

--- update_private_files.sh
+++ update_private_files.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+git add .
+git commit -m "Update privates"
 git checkout private
 git merge master -m "Update to master"
 git add .
 git commit -m "Add private files to branch"
 git checkout master
 git symbolic-ref HEAD refs/heads/master
-git reset HEAD .
+git reset
+cat >.gitignore <<EOL
+target/
+bin/
+*.jpg
+*.jpeg
+*.png
+*.pdf 
+EOL
