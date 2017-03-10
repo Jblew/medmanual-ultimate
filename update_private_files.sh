4ecd8ea0aa19b9db6f0b145a18a85d6f533e58ce
Jblew <jedrzejblew@gmail.com>
2017-03-10 14:06:48 +0100

    Update orivate script issue fixed

--- update_private_files.sh
+++ update_private_files.sh
@@ -6,7 +6,6 @@ git checkout private
 git merge master -m "Update to master"
 git add .
 git commit -m "Add private files to branch"
-git checkout master
 git symbolic-ref HEAD refs/heads/master
 git reset
 cat >.gitignore <<EOL
